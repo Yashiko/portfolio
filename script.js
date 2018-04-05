542132c79ab94b1839c9eb28b5b89672ac3c72fb
Yashiko <andriusgel@gmail.com>
2018-04-05 22:25:33 +0300

    Skills section
    nepilnai sutvarkytas skill sekcija. Truksta, kuomet nuscrolinam iki tos vietos ir pradeda veikti animacija

---
+++ script.js
@@ -0,0 +1,55 @@
+var bar = new ProgressBar.Circle(htmlSkill, {
+  strokeWidth: 6,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#555',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: null
+});
+
+bar.animate(0.8);  // Number from 0.0 to 1.0
+
+var bar1 = new ProgressBar.Circle(cssSkill, {
+  strokeWidth: 6,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#555',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: null
+});
+bar1.animate(0.8);  // Number from 0.0 to 1.0
+
+var bar3 = new ProgressBar.Circle(jsSkill, {
+  strokeWidth: 6,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#555',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: null
+});
+bar3.animate(0.5);  // Number from 0.0 to 1.0
+
+var bar4 = new ProgressBar.Circle(bootstrapSkill, {
+  strokeWidth: 6,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#555',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: null
+});
+bar4.animate(0.5);  // Number from 0.0 to 1.0
+
+var bar5 = new ProgressBar.Circle(jqSkill, {
+  strokeWidth: 6,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#555',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: null
+});
+bar5.animate(0.2);  // Number from 0.0 to 1.0
