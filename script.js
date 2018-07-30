5c629d94ced57c201f861ce64a323718e527ad03
Andrius <andriusgel@gmail.com>
2018-07-30 16:29:57 +0300

    tobulinamas puslapis
    Sutvarkytas, projektų lygiavimas

--- script.js
+++ script.js
@@ -1,58 +1,54 @@
-
-
-
 // Projects
 function showAll() {
     let resultHtmlWeb = `
-<div style ="width: 645px">
-    <div>
-    <a href="https://github.com/Yashiko/calculator" target="_blank">
-    <div class="container2">
-        <img class="projects-image" src="calculator.jpg" alt="calculator" width="300px">
-        <div class="overlay-projects">
-            <div class="text">
-                <em>"calcucator"
-                    <br>
-                    <p class="projects-list-text">by Andrius Geleževičius</p>
-                </em>
-            </div>
-        </div>
-    </div>
-    </a>
-    </div>
-
-  <div>
-  <a href="https://github.com/Yashiko/GameWall" target="_blank">
-      <div class="container2" >
-          <img class="projects-image" src="gameWall.jpg" alt="gameWall" width="300px">
-          <div class="overlay-projects">
-              <div class="text">
-                  <em>"gameWall"
-                      <br>
-                      <p class="projects-list-text">by Andrius Geleževičius</p>
-                  </em>
+    <div style ="width: 650px">
+      <div>
+      <a href="https://github.com/Yashiko/GameWall" target="_blank">
+          <div class="container2" >
+              <img class="projects-image" src="gameWall.jpg" alt="gameWall" width="300px">
+              <div class="overlay-projects">
+                  <div class="text">
+                      <em>"gameWall"
+                          <br>
+                          <p class="projects-list-text">by Andrius Geleževičius</p>
+                      </em>
+                  </div>
               </div>
           </div>
-      </div>
-  </a>
-
-  <a href="https://github.com/Yashiko/who-wants-to-be-a-millionaire-" target="_blank">
-      <div class="container2">
-          <img class="projects-image" src="mil.png" alt="millionaire" width="300px">
-          <div class="overlay-projects">
-              <div class="text">
-                  <em>"millionaire"
-                      <br>
-                      <p class="projects-list-text">by Andrius Geleževičius</p>
-                  </em>
-              </div>
+      </a>
+    
+      <a href="https://github.com/Yashiko/who-wants-to-be-a-millionaire-" target="_blank">
+          <div class="container2">
+              <img class="projects-image" src="mil.png" alt="millionaire" width="300px">
+              <div class="overlay-projects">
+                  <div class="text">
+                      <em>"millionaire"
+                          <br>
+                          <p class="projects-list-text">by Andrius Geleževičius</p>
+                      </em>
+                  </div>
+            </div>
+        </a>
         </div>
-    </a>
-    </div>
-
-</div>`;
-    document.querySelector("#result").innerHTML = resultHtmlWeb;
-};
+    
+        <div>
+        <a href="https://github.com/Yashiko/calculator" target="_blank">
+        <div class="container2">
+            <img class="projects-image" src="calculator.jpg" alt="calculator" width="300px">
+            <div class="overlay-projects">
+                <div class="text">
+                    <em>"calcucator"
+                        <br>
+                        <p class="projects-list-text">by Andrius Geleževičius</p>
+                    </em>
+                </div>
+            </div>
+        </div>
+        </a>
+        </div>
+    </div>`;
+        document.querySelector("#result").innerHTML = resultHtmlWeb;
+    };
 
 function showWeb() {
     let resultHtmlWeb = `<div></div>`;
@@ -77,7 +73,7 @@ function showWeb() {
 
 function showApp() {
     let resultHtmlWeb = `
-<div style ="width: 645px">
+<div style ="width: 650px">
   <div>
   <a href="https://github.com/Yashiko/GameWall" target="_blank">
       <div class="container2" >
